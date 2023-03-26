6d69b55d53094c3068772e249f8ed9456812bf1c
Brian 1143 <brian1143@gmail.com>
2023-03-26 19:06:39 +0800

    setup google admob

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:very_good_slide_puzzle/app/app.dart';
 import 'package:very_good_slide_puzzle/bootstrap.dart';
 
 void main() {
+  MobileAds.instance.initialize();
   bootstrap(() => const App());
 }
